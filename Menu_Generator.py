77d5390dbc39b6e8e72405c3f50dd9106a5d8b48
Lazan113 <106540207+pathit-set13@users.noreply.github.com>
2023-09-10 23:38:10 +0700

    Update Menu_Generator.py
    Functions are ok (for now)

--- Menu_Generator.py
+++ Menu_Generator.py
@@ -7,15 +7,13 @@ def type_random():
     list_type = ["ผัดกะเพรา", "ผัดพริกแกง", "คั่วพริกเกลือ", "ุสุกี้น้ำ", "สุกี้แห้ง", "ข้าวผัด", "ผัดผงกะหรี่",
                  "ทอดกระเทียม", "ผัดน้ำมันหอย"]
     food_type = random.choice(list_type)
-    print(food_type)
+    label_type.configure(text="Menu Type: "+food_type)
 
 
 def meat_random():
     list_meat = ["หมู", "หมูกรอบ", "หมูสับ", "ไก่", "เนื้อ", "หมึก", "กุ้ง", "ปลาดุก", "ปลาสลิด"]
     meat_type = random.choice(list_meat)
-    print(meat_type)
-
-#def combined_func():
+    label_meat.configure(text="Meat: "+meat_type)
 
 #def add_menu():
 #   typeAdd = input("Enter New Type: ")
@@ -25,8 +23,13 @@ def meat_random():
 
 MainWindow = Tk()
 label_Manu = Label(MainWindow,text="What should to eat?")
-label_Manu.grid(row=0, column=1)
-Random_Button = Button(MainWindow, text = "Random!", command = lambda:[type_random(),meat_random()] ).grid(row = 2, column = 1)
+label_Manu.grid(row=0, column=0)
+label_type = Label(MainWindow,text="")
+label_type.grid(row=1, column=0)
+label_meat = Label(MainWindow,text="")
+label_meat.grid(row=2, column=0)
+Random_Button = Button(MainWindow, text = "Random!", command = lambda:[type_random(),meat_random()])
+Random_Button.grid(row = 3, column = 0)
 
 MainWindow.mainloop()
 
